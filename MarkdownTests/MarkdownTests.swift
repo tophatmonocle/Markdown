192df0fda8e666852bc88f9466f9149159797350
Daniel Leping <dileping@gmail.com>
2016-03-01 19:08:23 +0200

    Markdown

---
+++ MarkdownTests/MarkdownTests.swift
@@ -0,0 +1,49 @@
+//===--- MarkdownTests.swift ----------------------------------------------------===//
+//
+//Copyright (c) 2015-2016 Daniel Leping (dileping)
+//
+//This file is part of Markdown.
+//
+//Swift Express is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Lesser General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//Swift Express is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public License
+//along with Swift Express.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import Markdown
+
+class MarkdownTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
